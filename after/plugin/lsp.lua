a88e03dcc01790c9f8284595fc958e319baa6f06
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2024-12-10 12:09:59 +0530

    finally configured lsp keymappings to work with Copilot

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,43 +1,50 @@
-local lsp = require('lsp-zero').preset('recommended')
+local lsp_zero = require('lsp-zero')
 
-lsp.ensure_installed({
-	'pyright',       -- Python
-	'ts_ls',      -- JavaScript/TypeScript
-	'eslint',				-- ESLint
-	'html',          -- HTML
-	'cssls',         -- CSS
-	'tailwindcss',   -- TailwindCSS
-	'jsonls',        -- JSON
-	'emmet_ls',      -- Emmet
-	'clangd',         -- C/C++
-})
-
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<CR>'] = cmp.mapping.confirm(cmp_select),
-	['<C-Space>'] = cmp.mapping.complete(),
-})
-
-lsp.set_preferences({
-	sign_icons = { }
-})
-
-lsp.on_attach(function(client, bufnr)
-	local opts = { buffer = bufnr, remap = false }
-
-	-- Keybindings for LSP
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-
-	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
-lsp.setup()
+-- Mason setup
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {
+    'pyright',        -- Python
+    'ts_ls',       -- TypeScript
+    'eslint',         -- JavaScript
+    'html',           -- HTML
+    'cssls',          -- CSS
+    'tailwindcss',    -- Tailwind CSS
+    'clangd',         -- C++
+  },
+  handlers = {
+    lsp_zero.default_setup,
+  },
+})
+
+-- nvim-cmp setup
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = true}),
+    
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+    
+    -- Navigate between snippet placeholder
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+  }),
+  
+  -- Enable autocomplete for all filetypes 
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'buffer' },
+    { name = 'path' },
+  })
+})
